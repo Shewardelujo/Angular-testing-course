28baf3935770c5e537fe3254a096adf6f120f413
Adesewa <adesewa.adefalujo@bankly.ng>
2024-05-07 13:37:40 +0100

    spy test

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -8,30 +8,21 @@ import { UtilsService } from './utils.service';
 //beforeEach is a block that will be executed before every single test in this file... If we need stuff in every single test, it makes sense to put it in beforeEach
 
 //with mock, we are using something else for our values
+//with spy, we are able to spy on the service and get the real values
 
 describe('UsersService', () => {
   //create a property which will be a reference to our UsersService
   let usersService: UsersService;
-
-  //mocking utilsService
-  const utilsServiceMock = {
-    pluck: jest.fn(),
-  };
+  let utilsService: UtilsService;
 
   beforeEach(() => {
     // configureTestingModule creates a testing module (which is like a simple ng module, where you can use things like import, providers etc)
     TestBed.configureTestingModule({
       //we want to register the service in our module
-      providers: [
-        UsersService,
-        {
-          //mocking utilsService
-          provide: UtilsService,
-          useValue: utilsServiceMock,
-        },
-      ],
+      providers: [UsersService, UtilsService],
     });
     usersService = TestBed.inject(UsersService);
+    utilsService = TestBed.inject(UtilsService);
   });
 
   it('creates a service', () => {
@@ -69,9 +60,18 @@ describe('UsersService', () => {
 
   describe('getUsernames', () => {
     it('should get usernames', () => {
-      //when you mock a service, it returns nothing... so, the pluck function does nothing and returns undefined and the test fail... However, we can provide a mock value, so that instead of returning undefined, it returns the stated value, irregardless of the value passed to it... see this in A) below
-      utilsServiceMock.pluck.mockReturnValue(['fooloo']); //A) fooloo will always be returned here
-      expect(usersService.getUsernames()).toEqual(['foo']);
+      jest.spyOn(utilsService, 'pluck'); //so utilService is the real one and we are spying on the pluck function
+      usersService.users = [
+        {
+          id: '3',
+          name: 'foo',
+        },
+      ];
+      usersService.getUsernames();
+      expect(utilsService.pluck).toHaveBeenCalledWith(
+        usersService.users,
+        'name'
+      );
     });
   });
 });
