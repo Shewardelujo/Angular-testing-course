c0b61bf2cdbd64885a1801bd75eecf3034e5cbfc
Adesewa <adesewa.adefalujo@bankly.ng>
2024-05-07 12:45:04 +0100

    mock test

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -7,15 +7,29 @@ import { UtilsService } from './utils.service';
 
 //beforeEach is a block that will be executed before every single test in this file... If we need stuff in every single test, it makes sense to put it in beforeEach
 
+//with mock, we are using something else for our values
+
 describe('UsersService', () => {
   //create a property which will be a reference to our UsersService
   let usersService: UsersService;
+
+  //mocking utilsService
+  const utilsServiceMock = {
+    pluck: jest.fn(),
+  };
 
   beforeEach(() => {
     // configureTestingModule creates a testing module (which is like a simple ng module, where you can use things like import, providers etc)
     TestBed.configureTestingModule({
       //we want to register the service in our module
-      providers: [UsersService, UtilsService],
+      providers: [
+        UsersService,
+        {
+          //mocking utilsService
+          provide: UtilsService,
+          useValue: utilsServiceMock,
+        },
+      ],
     });
     usersService = TestBed.inject(UsersService);
   });
@@ -50,6 +64,14 @@ describe('UsersService', () => {
       ];
       usersService.removeUser('3');
       expect(usersService.users).toEqual([]);
+    });
+  });
+
+  describe('getUsernames', () => {
+    it('should get usernames', () => {
+      //when you mock a service, it returns nothing... so, the pluck function does nothing and returns undefined and the test fail... However, we can provide a mock value, so that instead of returning undefined, it returns the stated value, irregardless of the value passed to it... see this in A) below
+      utilsServiceMock.pluck.mockReturnValue(['fooloo']); //A) fooloo will always be returned here
+      expect(usersService.getUsernames()).toEqual(['foo']);
     });
   });
 });
